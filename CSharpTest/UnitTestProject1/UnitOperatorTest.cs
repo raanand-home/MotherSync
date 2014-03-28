e7dcf8909ea1ea11f80bac9da3fdd8500a491d2e
denisRuth <denisRuti@gmail.com>
2014-03-28 11:16:35 +0300

    add test

---
+++ CSharpTest/UnitTestProject1/UnitOperatorTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    public class Complex
+    {
+        public double Real { get; set;}
+        public double Imaginary { get; set; }
+        // binary operator overloading
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            return new Complex() { Real = c1.Real + c2.Real, Imaginary = c1.Imaginary + c2.Imaginary };
+        }
+
+//        public static Complex operator + (Complex c1)
+//        {
+//              return new Complex() { Real = this.Real + c1.Real, Imaginary = this.Imaginary + c2.Imaginary };
+//        }
+    
+    }
+    [TestClass]
+    public class UnitOperatorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
