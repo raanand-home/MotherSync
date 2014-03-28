399a52ed26920b9a0838bedbc65b34e70a6b8a77
raanan <raanand@gmail.com>
2014-03-28 11:27:19 +0300

    Add TestDelegate

---
+++ CSharpTest/UnitTestProject1/TestDelegates.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    delegate void EmptyFunc();
+    [TestClass]
+    public class TestDelegates
+    {
+        int Num = 0;
+        void IncressNumber()
+        {
+            Num++;
+        }
+        [TestMethod]
+        public void TestDelegate()
+        {
+            EmptyFunc functionPointer = null;
+
+            if (functionPointer != null)
+            {
+                Assert.Fail("this shouldn't happend");
+            }
+            //Assiment
+            functionPointer = IncressNumber;
+            
+            //Invoke
+            functionPointer();
+
+            Assert.AreEqual(Num, 1);
+
+        }
+    }
+}
