45c5f0de144a69e4225888eecf4053f636cbaca9
raanand-home <raanand@gmail.com>
2014-03-31 19:38:53 +0300

    Add Chat Unit Test

---
+++ CSharpTest/ChatUnitTest/TCPServerClientUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChatUnitTest
+{
+    [TestClass]
+    public class TCPServerClientUnitTest
+    {
+        [TestMethod]
+        public void ListenTest()
+        {
+            // Create Tcp Server
+            // Listen To New Client
+            // Close
+        }
+
+        [TestMethod]
+        public void SendToServerTest()
+        {
+            // Create Tcp Server
+            // Listen To New Client
+            
+            // Create Client
+            // Connect Client To Server
+            // Send Message To Server
+            // Close
+        }
+
+        [TestMethod]
+        public void SendToServerTest()
+        {
+            // Create Tcp Server
+            // Listen To NewClient
+
+            // Create Client
+            // Connect Client To Server
+            // Send Message from Server to the client
+            // Close
+        }
+    }
+
+    
+}
